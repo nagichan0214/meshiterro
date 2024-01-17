1bcb663dd7f5738457fac2200c15c90718dc87de
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-01-17 19:13:58 +0000

    [Finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,11 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+   has_many :favorites, dependent: :destroy
+  
+   validates :shop_name, presence: true
+    validates :image, presence: true
   
   def get_image
     if image.attached?
@@ -18,5 +23,7 @@ class PostImage < ApplicationRecord
     end
     image
   end
-
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
